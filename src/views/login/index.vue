676f95330fb4d13684f3c9cb39907ef1db2033c4
黄龙 <122418842@qq.com>
2023-05-21 17:16:47 +0800

    p13 登录注册-准备-布局结构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,40 @@
 <template>
-  <div clas="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar title="登录" />
+    <!--/ 导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        name="手机号"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        type="password"
+        name="验证码"
+        placeholder="请输入验证"
+      />
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+    <!--/ 登录表单 -->
+  </div>
 </template>
 
 <script>
 export default {
   name: 'LoginIndex',
   data() {
-    return {}
+    return {
+    }
   },
-  methods: {}
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
 }
 </script>
 

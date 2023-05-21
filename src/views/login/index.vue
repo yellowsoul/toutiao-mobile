8815b7ac89adcdbe65a62904769be58388840de3
黄龙 <122418842@qq.com>
2023-05-21 18:31:25 +0800

    p14 登录注册-准备-布局样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="login-container">
     <!-- 导航栏 -->
-    <van-nav-bar title="登录" />
+    <van-nav-bar class="page-nav-bar" title="登录" />
     <!--/ 导航栏 -->
 
     <!-- 登录表单 -->
     <van-form @submit="onSubmit">
       <van-field
+        left-icon="smile-o"
         name="手机号"
         placeholder="请输入手机号"
-      />
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
       <van-field
         type="password"
         name="验证码"
         placeholder="请输入验证"
-      />
-      <div style="margin: 16px">
-        <van-button block type="info" native-type="submit">登录</van-button>
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <div class="send-wrap">
+            <i class="send-line"></i>
+            <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+          </div>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">登录</van-button>
       </div>
     </van-form>
     <!--/ 登录表单 -->
@@ -38,4 +49,34 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login-container {
+  .toutiao{
+    font-size:38px;
+  }
+  .send-wrap{
+    display: flex;
+    align-items: center;
+  }
+  .send-line{
+    display: inline-block;
+    width:1px;
+    height:40px;
+    background-color:#ededed;
+    margin:0 20px;
+  }
+  .send-sms-btn{
+    background-color: #ededed;
+    height:46px;
+    line-height: 46px;
+    font-size:22px;
+  }
+  .login-btn-wrap{
+    padding: 53px 33px;
+    .login-btn{
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>

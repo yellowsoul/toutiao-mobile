0f6f7ff31f9639f4bf15e4b2881767b3286cc16b
黄龙 <122418842@qq.com>
2023-05-22 16:33:27 +0800

    个人中心-页面布局-顶部未登录

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="profile-container">我的</div>
+  <div class="profile-container">
+    <div class="header not-login">
+      <div class="login-btn" @click="$router.push('/login')">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="" />
+        <span class="text">登录 / 注册</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,4 +19,33 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.profile-container {
+  .header {
+    height: 360px;
+    background: url('~@/assets/banner.png');
+    background-size: cover;
+  }
+
+  .not-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .mobile-img {
+        width: 132px;
+        height: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>

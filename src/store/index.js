cf64ca4826d5e625f31ac9f8b405811d4d5fbd33
黄龙 <122418842@qq.com>
2023-05-22 14:43:32 +0800

    登录注册-封装本地存储操作模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
@@ -8,14 +9,16 @@ const TOKEN_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
     // 一个对象，存储当前登录用户信息 （token等信息）
-    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY)) || {}
+    user: getItem(TOKEN_KEY) || {}
+    // user: JSON.parse(window.localStorage.getItem(TOKEN_KEY)) || {}
   },
   mutations: {
     setUser(state, data) {
       state.user = data
 
       // 为了防止刷新丢失，我们需要把数据备份到本地存储
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
+      // window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
     }
   },
   actions: {

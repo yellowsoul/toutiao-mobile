aa0a532a76e46ecdd84d19431c3a2decb8dd5e41
黄龙 <122418842@qq.com>
2023-05-22 17:08:37 +0800

    13 个人中心-页面布局-宫格导航

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -6,6 +6,53 @@
         <span class="text">登录 / 注册</span>
       </div>
     </div>
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+            round
+            fit="cover"
+          />
+          <span class="name">黑马头条号</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round>编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- 宫格导航 -->
+    <van-grid class="grid-nav" :column-num="2" clickable>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-shoucang"></i>
+        <span slot="text" class="text">收藏</span>
+      </van-grid-item>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-lishi"></i>
+        <span slot="text" class="text">历史</span>
+      </van-grid-item>
+    </van-grid>
+    <!-- /宫格导航 -->
   </div>
 </template>
 
@@ -44,6 +91,67 @@ export default {
       .text {
         font-size: 28px;
         color: #fff;
+      }
+    }
+  }
+  .user-info {
+    .base-info {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 230px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          margin-right: 23px;
+          border:4px solid #fff;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      // height: 130px;
+      .data-item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        flex: 1;
+        height: 130px;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 24px;
+        }
+      }
+    }
+  }
+
+  .grid-nav {
+    .grid-item {
+      height: 140px;
+      i.toutiao{
+        font-size: 46px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
       }
     }
   }

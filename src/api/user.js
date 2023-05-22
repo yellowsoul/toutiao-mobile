893653dbb008f375b432032a130e44f39bec091f
黄龙 <122418842@qq.com>
2023-05-22 19:01:08 +0800

    17 个人中心-优化设置Token

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 
 // 登录
 export const login = (data) => {
@@ -30,12 +30,12 @@ export const sendSms = (mobile) => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    // 发送请求头数据
-    headers: {
-      // 注意:该接口需要授权才能访问
-      //    token的数据格式 Bearer token数据,注意 Bearer 后面有个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
+    // 发送请求头数据 - > 统一在axios请求头里去添加
+    // headers: {
+    //   // 注意:该接口需要授权才能访问
+    //   //    token的数据格式 Bearer token数据,注意 Bearer 后面有个空格
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,13 +2,28 @@
  * 请求模块
  */
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   baseURL: 'http://api-toutiao-web.itheima.net' // 接口的基准路径
 })
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  // 请求发起会经过这里
+  // config 配置对象:本次请求的配置对象
+  // console.log(config)
+  const { user } = store.state
+  if (user && user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
 
+  // 注意:这里务必要返回 config 配置对象,否则请求就停在这里出不去了
+  return config // 放行的标记
+}, function (error) {
+  // 如果请求出错了(还没有发出去)会进入这里
+  return Promise.reject(error)
+})
 // 响应拦截器
 
 export default request

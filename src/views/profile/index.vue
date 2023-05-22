4b109ffa8c7d838eeddae2dcac003bbdee021652
黄龙 <122418842@qq.com>
2023-05-22 18:37:08 +0800

    个人中心-展示当前登录用户信息

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -4,13 +4,8 @@
     <div v-if="user" class="header user-info">
       <div class="base-info">
         <div class="left">
-          <van-image
-            class="avatar"
-            src="https://img01.yzcdn.cn/vant/cat.jpeg"
-            round
-            fit="cover"
-          />
-          <span class="name">黑马头条号</span>
+          <van-image class="avatar" :src="userInfo.photo" round fit="cover" />
+          <span class="name">{{ userInfo.name }}</span>
         </div>
         <div class="right">
           <van-button size="mini" round>编辑资料</van-button>
@@ -18,19 +13,19 @@
       </div>
       <div class="data-stats">
         <div class="data-item">
-          <span class="count">10</span>
+          <span class="count">{{ userInfo.art_count }}</span>
           <span class="text">头条</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
+          <span class="count">{{ userInfo.follow_count }}</span>
           <span class="text">关注</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
+          <span class="count">{{ userInfo.fans_count }}</span>
           <span class="text">粉丝</span>
         </div>
         <div class="data-item">
-          <span class="count">10</span>
+          <span class="count">{{ userInfo.like_count }}</span>
           <span class="text">获赞</span>
         </div>
       </div>
@@ -73,16 +68,26 @@
 
 <script>
 import { mapState } from 'vuex'
+import { getUserInfo } from '@/api/user'
 
 export default {
   name: 'ProfileIndex',
   data() {
-    return {}
+    return {
+      userInfo: {} // 用户信息
+    }
   },
   computed: {
     ...mapState(['user'])
   },
+  created() {
+    // 如果用户登录了,则请求加载用户信息数据
+    if (this.user) {
+      this.loadUserInfo()
+    }
+  },
   methods: {
+    // 退出登录
     onLogout() {
       // 退出提示
       // 在组件中需要使用 this.$dialog 来调用弹框组件
@@ -99,6 +104,15 @@ export default {
           // on cancel
           console.log('取消')
         })
+    },
+
+    async loadUserInfo() {
+      try {
+        const { data } = await getUserInfo()
+        this.userInfo = data.data
+      } catch (err) {
+        this.$toast('获取数据失败,请稍后重试')
+      }
     }
   }
 }

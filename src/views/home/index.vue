255086f621bcf1fd3bc57e6ec90b235fa4585961
黄龙 <122418842@qq.com>
2023-05-23 21:11:07 +0800

    02 首页-处理文章频道列表-使用Tab标签页组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,6 +13,20 @@
       >
     </van-nav-bar>
     <!--/ 导航栏 -->
+
+    <!-- 频道列表 -->
+    <!--
+      通过 v-model 绑定当前激活标签对应的索引值，默认情况下启用第一个标签。
+      通过 animated 属性可以开启切换标签内容时的转场动画。
+      通过 swipeable 属性可以开启滑动切换标签页。
+    -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- /频道列表 -->
   </div>
 </template>
 
@@ -20,7 +34,9 @@
 export default {
   name: 'HomeIndwx',
   data() {
-    return {}
+    return {
+      active: 0
+    }
   },
   methods: {}
 }

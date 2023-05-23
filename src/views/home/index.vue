d9dd73fab9e43dfedd790fca093aaaed298709ea
黄龙 <122418842@qq.com>
2023-05-23 19:16:04 +0800

    01 首页-页面布局-头部导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+    <!--/ 导航栏 -->
+  </div>
 </template>
 
 <script>
@@ -12,4 +26,24 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
+</style>
+<style lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+}
+</style>

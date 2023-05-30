2bd48cc8918d5ae0bf402eb530fa936b98306c76
黄龙 <122418842@qq.com>
2023-05-30 10:58:54 +0800

    01 首页文章列表-样式调整

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,6 +1,6 @@
 <template>
   <van-cell class="article-item">
-    <div slot="title">{{ article.title }}</div>
+    <div slot="title" class="title van-multi-ellipsis--l2">{{ article.title }}</div>
     <div slot="label">
       <!-- 三张图 -->
       <div v-if="article.cover.type === 3" class="cover-wrap">
@@ -9,10 +9,10 @@
           v-for="(img, index) in article.cover.images"
           :key="index"
         >
-          <van-image width="100" height="100" :src="img" />
+          <van-image class="cover-item-img" :src="img" fit="cover" />
         </div>
       </div>
-      <div>
+      <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
         <span>{{ article.comm_count }}评论</span>
         <span>{{ article.pubdate }}</span>
@@ -22,8 +22,8 @@
     <van-image
       v-if="article.cover.type === 1"
       slot="default"
-      width="100"
-      height="100"
+      class="right-cover"
+      fit="cover"
       :src="article.cover.images[0]"
     />
   </van-cell>
@@ -45,4 +45,45 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.article-item {
+  .title {
+    font-size: 32px;
+    color:#3a3a3a;
+  }
+
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 24px;
+  }
+
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex:1;
+      height: 146px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
+</style>

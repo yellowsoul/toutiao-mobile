6db2a9a1ffc6c5fb56073eeeea08fe70688f64c7
黄龙 <122418842@qq.com>
2023-05-31 22:26:32 +0800

    频道编辑-正确的获取展示首页频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -69,6 +69,8 @@
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list.vue'
 import ChannelEdit from './components/channel-edit.vue'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 
 export default {
   name: 'HomeIndwx',
@@ -83,14 +85,37 @@ export default {
       isChennelEditShow: false // 控制编辑频道弹出层的显示状态
     }
   },
+  computed: {
+    ...mapState('user')
+  },
   created() {
     this.loadChannels()
   },
   methods: {
     async loadChannels() {
       try {
-        const { data } = await getUserChannels()
-        this.channels = data.data.channels
+        // const { data } = await getUserChannels()
+        // this.channels = data.data.channels
+        let channels = []
+
+        if (this.user) {
+          // 已登录，请求获取用户频道列表
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        } else {
+          // 未登录，判断是否有本地的频道列表数据
+          const localChannels = getItem('TOUTIAO_CHANNELS')
+          //    有，拿来使用
+          if (localChannels) {
+            channels = localChannels
+          } else {
+            //    没有，请求获取默认频道列表
+            const { data } = await getUserChannels()
+            channels = data.data.channels
+          }
+        }
+
+        this.channels = channels
       } catch (err) {
         this.$toast('获取频道数据失败')
       }

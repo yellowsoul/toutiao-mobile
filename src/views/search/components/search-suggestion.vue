6afe71d5599610d40959698bc5661577da678803
黄龙 <122418842@qq.com>
2023-06-01 23:24:10 +0800

    文章搜索-联想建议-获取并监视输入框内容变化

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -11,8 +11,25 @@
 <script>
 export default {
   name: 'SearchSuggestion',
+  props: {
+    searchText: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {}
+  },
+  watch: {
+    searchText: {
+      // 当 searchText 发生改变的时候就会调用 handler 函数
+      // 注意：handler 函数名称是固定的
+      handler(value) {
+        console.log(value)
+      },
+
+      immediate: true // 该回调将会在侦听开始之后被立即调用
+    }
   },
   methods: {}
 }
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -22,7 +22,10 @@
     <!-- /历史结果 -->
 
     <!-- 联想建议 -->
-    <search-suggestion v-else-if="searchText" />
+    <search-suggestion
+      v-else-if="searchText"
+      :search-text="searchText"
+    />
     <!-- /联想建议 -->
 
     <!-- 搜索历史记录 -->

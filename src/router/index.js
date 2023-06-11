56a12a4c347ef0391769985610c6712bfb84455b
黄龙 <122418842@qq.com>
2023-06-11 11:12:27 +0800

    文章详情-创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,11 @@ const routes = [
     path: '/search',
     name: 'search',
     component: () => import('@/views/search')
+  },
+  {
+    path: '/article/:articleId',
+    name: 'article',
+    component: () => import('@/views/article')
   }
 ]
 
---
+++ src/views/article/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="article-container">文章详情页</div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleIndex',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>

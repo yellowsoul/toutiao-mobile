227d86618b83da39a01e2a51c1c8a0951b467f2b
黄龙 <122418842@qq.com>
2023-06-12 19:26:43 +0800

    文章详情-文章点赞（处理完成）

--- src/components/like-article/index.vue
+++ src/components/like-article/index.vue
@@ -3,12 +3,12 @@
     :color="value ===1 ? '#e5645f' : '#777'"
     :name="value ===1 ? 'good-job' : 'good-job-o'"
     :loading="loading"
-    @click="onCollect"
+    @click="onLike"
   />
 </template>
 
 <script>
-// import { addCollect, deleteCollect } from '@/api/article'
+import { addLike, deleteLike } from '@/api/article'
 export default {
   name: 'LikeArticle',
   props: {
@@ -27,8 +27,25 @@ export default {
     }
   },
   methods: {
-    async onCollect() {
+    async onLike() {
       this.loading = true
+      try {
+        let status = -1
+        if (this.value === 1) {
+          // 已点赞，取消点赞
+          await deleteLike(this.articleId)
+        } else {
+          // 没有点赞，添加点赞
+          await addLike(this.articleId)
+          status = 1
+        }
+        // 更新视图
+        this.$emit('input', status)
+        this.$toast.success(status === 1 ? '点赞成功' : '取消点赞')
+      } catch (err) {
+        this.$toast.fail('操作失败，请重试！')
+      }
+      this.loading = false
     }
   }
 }

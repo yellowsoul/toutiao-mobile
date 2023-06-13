8e950b0566d06b7c42dacab3e6a09eef5fc19f7e
黄龙 <122418842@qq.com>
2023-06-13 20:59:17 +0800

    编辑用户资料-页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="user-profile">用户资料</div>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    ></van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 个人信息 -->
+    <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        round
+        src="https://img01.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link/>
+    <van-cell title="性别" value="内容" is-link/>
+    <van-cell title="生日" value="内容" is-link/>
+    <!-- /个人信息 -->
+  </div>
 </template>
 
 <script>
@@ -12,4 +34,11 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.user-profile {
+  .avatar{
+    width: 60px;
+    height: 60px;
+  }
+}
+</style>

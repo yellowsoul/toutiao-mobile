8c6902814dee93ccc61a6c0b2247e534eb27ed98
黄龙 <122418842@qq.com>
2023-06-13 22:12:19 +0800

    编辑用户资料-修改昵称-处理完成

--- src/views/user-profile/components/update-name.vue
+++ src/views/user-profile/components/update-name.vue
@@ -6,13 +6,14 @@
       left-text="取消"
       right-text="完成"
       @click-left="$emit('close')"
+      @click-right="onConfirm"
     ></van-nav-bar>
     <!-- /导航栏 -->
 
     <!-- 输入框 -->
     <div class="field-wrap">
       <van-field
-        v-model="localName"
+        v-model.trim="localName"
         rows="2"
         autosize
         type="textarea"
@@ -26,6 +27,8 @@
 </template>
 
 <script>
+import { updateUserProfile } from '@/api/user'
+
 export default {
   name: 'UpdateName',
   props: {
@@ -39,7 +42,35 @@ export default {
       localName: this.value
     }
   },
-  methods: {}
+  methods: {
+    async onConfirm() {
+      this.$toast.loading({
+        message: '保存中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续展示
+      })
+      try {
+        const localName = this.localName
+        if (!localName.length) {
+          this.$toast('昵称不能为空')
+          return
+        }
+
+        await updateUserProfile({
+          name: localName
+        })
+
+        // 更新视图
+        this.$emit('input', localName)
+        // 关闭弹层
+        this.$emit('close')
+        // 提示成功
+        this.$toast.success('更新成功')
+      } catch (err) {
+        this.$toast.fail('更新失败')
+      }
+    }
+  }
 }
 </script>
 

9604f4df2cbc4a568be645502719e3714a7c9d25
黄龙 <122418842@qq.com>
2023-06-16 17:27:00 +0800

    编辑用户资料-修改头像-图片裁切（初始化）

--- src/views/user-profile/components/update-photo.vue
+++ src/views/user-profile/components/update-photo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="update-photo">
-    <img class="img" :src="img" />
+    <img class="img" :src="img" ref="img" />
     <div class="toolbar">
       <div class="cancel" @click="$emit('close')">取消</div>
       <div class="confirm">完成</div>
@@ -9,6 +9,10 @@
 </template>
 
 <script>
+// 图片裁切插件
+import 'cropperjs/dist/cropper.css'
+import Cropper from 'cropperjs'
+
 export default {
   name: 'UpdatePhoto',
   props: {
@@ -19,6 +23,22 @@ export default {
   },
   data() {
     return {}
+  },
+  mounted() {
+    const image = this.$refs.img
+    const cropper = new Cropper(image, {
+      aspectRatio: 16 / 9,
+      crop(event) {
+        console.log(event.detail.x)
+        console.log(event.detail.y)
+        console.log(event.detail.width)
+        console.log(event.detail.height)
+        console.log(event.detail.rotate)
+        console.log(event.detail.scaleX)
+        console.log(event.detail.scaleY)
+      }
+    })
+    console.log(cropper)
   },
   methods: {}
 }
@@ -35,18 +55,20 @@ export default {
     bottom: 0;
     display: flex;
     justify-content: space-between;
-    .cancel, .confirm {
+    .cancel,
+    .confirm {
       width: 90px;
       height: 90px;
       font-size: 30px;
       display: flex;
       justify-content: center;
       align-items: center;
-      color:#fff;
+      color: #fff;
     }
   }
 }
 .img {
+  display: block;
   max-width: 100%;
 }
 </style>

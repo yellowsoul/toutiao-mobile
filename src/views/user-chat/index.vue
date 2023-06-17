122741ef6d1e8ab8789ff599d1df85e55cddcdf0
黄龙 <122418842@qq.com>
2023-06-17 16:59:09 +0800

    ay11-06 小智同学-收发消息

--- src/views/user-chat/index.vue
+++ src/views/user-chat/index.vue
@@ -11,9 +11,12 @@
     <!-- /导航栏 -->
 
     <!-- 消息列表 -->
-    <van-cell-group class="message-list">
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
+    <van-cell-group class="message-list" ref="message-list">
+      <van-cell
+        :title="item.msg"
+        v-for="(item, index) in messages"
+        :key="index"
+      />
     </van-cell-group>
     <!-- /消息列表 -->
 
@@ -35,23 +38,84 @@
 </template>
 
 <script>
+import { io } from 'socket.io-client'
+import { getItem, setItem } from '@/utils/storage'
+
 export default {
   name: 'UserChat',
   data() {
     return {
-      message: ''
+      message: '',
+      socket: null, // WebSocket 通信对象
+      messages: getItem('chat-messages') || [] // 消息列表
     }
   },
 
   computed: {},
 
-  watch: {},
+  watch: {
+    messages() {
+      setItem('chat-messages', this.messages)
 
-  created() {},
+      // 如果你要在操作数据之后立即操作数据影响的视图DOM，那么最好把代码放到 nextTick 函数中
+      // 数据改变影响视图更新这件事儿不是立即的
+      this.$nextTick(() => {
+        this.scrollToBottom()
+      })
+    }
+  },
 
-  mounted() {},
+  created() {
+    // 建立连接
+    const socket = io('http://ttapi.research.itcast.cn')
+    // const socket = io('http://api-toutiao-web.itheima.net')
+    this.socket = socket
 
-  methods: {}
+    socket.on('connect', () => {
+      console.log('连接建立成功了')
+    })
+
+    // 断开连接了
+    socket.on('disconnect', () => {
+      console.log('断开连接了')
+    })
+
+    // 发送消息
+    // socket.emit('消息类型'，消息内容)
+    // socket.on('消息类型'，function(data) { data消息 })
+
+    // 监听 message 事件，接收服务端消息
+    socket.on('message', (data) => {
+      // 把对方发给我的消息放到数中
+      this.messages.push(data)
+    })
+  },
+
+  mounted() {
+    this.scrollToBottom()
+  },
+
+  methods: {
+    onSend() {
+      // 请求发送消息
+      const data = {
+        msg: this.message,
+        timestamp: Date.now()
+      }
+      this.socket.emit('message', data)
+
+      // 把用户发出去的消息存储到数组中
+      this.messages.push(data)
+
+      // 清空输入框
+      this.message = ''
+    },
+
+    scrollToBottom() {
+      const list = this.$refs['message-list']
+      list.scrollTop = list.scrollHeight
+    }
+  }
 }
 </script>
 
